8bb7824d35d8e68320c17d5b6fe4a104ab882751
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-01-15 12:40:30 +0530

    replace a list of values

---
+++ d18.3.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df = pd.DataFrame({
+    'score':['exceptional','average','good','poor','average','exceptional'],
+    'student':['rob','maya','parthiv','tom','julian','erica']
+})
+new_df=df.replace(['poor','average','good','exceptional'],[1,2,3,4])
+print(new_df)

c1fdcebaab813e59d7e0a4bf22bda82376ac62e5
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-01-15 21:02:21 +0530

    pivot table continuous

--- d26.py
+++ d26.py
@@ -2,18 +2,20 @@ import pandas as pd
 
 df = pd.read_csv("d25.csv")
 
-new_df = df.pivot_table(index="city",columns="day")
+new_df = df.pivot_table(index="city",columns="day",aggfunc="mean")
 print(new_df)
-#            humidity            temperature
-# day      05/01/2027 05/02/2017  05/01/2027 05/02/2017
-# city
-# mumbai         81.5       55.5        76.5       81.0
-# new york       60.0       61.0        63.0       71.0
 
-new_df = df.pivot_table(index="city",columns="day",aggfunc="sum")
+new_df = df.pivot_table(index="city",columns="day",aggfunc="diff")
 print(new_df)
-#            humidity            temperature
-# day      05/01/2027 05/02/2017  05/01/2027 05/02/2017
+
+new_df = df.pivot_table(index="city",columns="day",aggfunc="count")
+print(new_df)
+
+new_df = df.pivot_table(index="city",columns="day",margins=True)
+print(new_df)
+#            humidity                  temperature
+# day      05/01/2027 05/02/2017   All  05/01/2027 05/02/2017     All
 # city
-# mumbai        163.0      111.0       153.0      162.0
-# new york      120.0      122.0       126.0      142.0
+# mumbai        81.50      55.50  68.5       76.50       81.0  78.750
+# new york      60.00      61.00  60.5       63.00       71.0  67.000
+# All           70.75      58.25  64.5       69.75       76.0  72.875
